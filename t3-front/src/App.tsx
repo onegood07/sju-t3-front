b0fede014382e38bff34d98dfd039a034c59d8d4
onegood07 <one_good07@naver.com>
2025-11-19 10:56:58 +0900

    setting/#1 : react-router-dom 추가

--- t3-front/src/App.tsx
+++ t3-front/src/App.tsx
@@ -1,30 +1,14 @@
-import { useState } from "react";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<div>Home</div>} />
+        <Route path="/report" element={<div>Report</div>} />
+        <Route path="/chatting" element={<div>Chatting</div>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
