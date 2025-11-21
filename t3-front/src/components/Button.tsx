6bbf56d29d89147f5cdb01bb9bf6f70e4116ea87
onegood07 <one_good07@naver.com>
2025-11-21 23:00:46 +0900

    feat/#5 : 공통 컴포넌트 Button 수정

--- t3-front/src/components/Button.tsx
+++ t3-front/src/components/Button.tsx
@@ -1,46 +1,55 @@
 import React from "react";
 
-type ButtonVariant = "primary" | "secondary" | "gray" | "outline";
+type ButtonVariant = "primary" | "login" | "outLine" | "grayBg";
 
 interface ButtonProps {
-    children: React.ReactNode;
-    onClick?: () => void;
-    className?: string;
-    disabled?: boolean;
-    variant?: ButtonVariant;
-    type?: "button" | "submit" | "reset";
+  children: React.ReactNode;
+  onClick?: () => void;
+  className?: string;
+  disabled?: boolean;
+  variant?: ButtonVariant;
+  type?: "button" | "submit" | "reset";
+  icon?: React.ReactNode;
 }
 
 export default function Button({
-    children,
-    onClick,
-    className = "",
-    disabled = false,
-    type = "button",
+  children,
+  onClick,
+  className = "",
+  disabled = false,
+  type = "button",
+  variant = "primary",
+  icon,
 }: ButtonProps) {
-    const baseStyles =
-        "w-full py-3 rounded-xl font-semibold text-center transition active:scale-[0.98]";
+  const baseStyles =
+    "w-full py-3 rounded-xl font-semibold text-center transition active:scale-[0.98]";
 
-    const primary = `
-    bg-primary-green 
-    text-white-default 
-    shadow-sm 
-    hover:bg-[#00c53f]
-  `;
+  const variantClasses = {
+    primary:
+      "bg-primary-green border-primary-green text-white-default hover:bg-text-green hover:border-primary-green",
+    login:
+      "bg-primary-yellow border-primary-yellow text-text-primary hover:bg-hover-yellow hover:border-hover-yellow",
+    outLine:
+      "bg-white-default text-text-green border-text-green hover:bg-bg-tag-success hover:border-text-green",
+    grayBg:
+      "bg-bg-input text-text-gray hover:bg-icon-gray hover:border-icon-gray",
+  };
 
-    return (
-        <button
-            type={type}
-            onClick={onClick}
-            disabled={disabled}
-            className={`
+  return (
+    <button
+      type={type}
+      onClick={onClick}
+      disabled={disabled}
+      className={`flex items-center justify-center gap-2 ${
+        variantClasses[variant]
+      }
         ${baseStyles}
-        ${primary}
         ${disabled ? "opacity-60 cursor-not-allowed" : ""}
         ${className}
       `}
-        >
-            {children}
-        </button>
-    );
+    >
+      {icon && icon}
+      {children}
+    </button>
+  );
 }
